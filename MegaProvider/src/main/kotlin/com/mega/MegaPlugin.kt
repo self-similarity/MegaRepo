af126031e5ab119de20ebce823742563543d98e3
self-similarity <137652432+self-similarity@users.noreply.github.com>
2023-08-04 02:14:50 +0000

    Update MegaPlugin.kt

--- MegaProvider/src/main/kotlin/com/mega/MegaPlugin.kt
+++ MegaProvider/src/main/kotlin/com/mega/MegaPlugin.kt
@@ -44,6 +44,7 @@ class MegaPlugin : Plugin() {
         val text =
             app.get("https://raw.githubusercontent.com/recloudstream/cs-repos/master/repos-db.json").text
 
+        // To parse both objects and strings
         val tree = ObjectMapper().readTree(text)
 
         return tree.mapNotNull {
